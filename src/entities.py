88652481010c69c363a113ea228295c739797704
Denys Rógeres <dennysrogeres@gmail.com>
2022-09-04 00:29:00 -0300

    feat: add entities
    Co-authered-by Percon12 <fcorreiaandrade@gmail.com>
    Co-authered-by mateusbrandaot <mateus.brandaoteixeira@gmail.com>

---
+++ src/entities.py
@@ -0,0 +1,17 @@
+from mesa import Agent
+
+class Entity(Agent):
+    grid = None
+    x = None
+    y = None
+    moore = True
+
+    def __init__(self, unique_id, pos, model, moore=True):
+        super().__init__(unique_id, model)
+        self.pos = pos
+        self.moore = moore
+
+    def random_move(self):
+        next_moves = self.model.grid.get_neighborhood(self.pos, self.moore, True)
+        next_move = self.random.choice(next_moves)
+        self.model.grid.move_agent(self,next_move)
