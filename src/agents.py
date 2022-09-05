54f0bd33a56613d2e5b41d64b33cf7d98b2435b4
luucas-melo <lucasmelodos322@gmail.com>
2022-09-05 03:13:33 -0300

    fix method and add denounced attr

--- src/agents.py
+++ src/agents.py
@@ -7,12 +7,12 @@ class Thief(Entity):
 
     def step(self):
         self.random_move()
-
-        this_cell = self.model.grid.get_cell_list_contents([self.pos])
-        person = [obj for obj in this_cell if isinstance(obj, Person)][0]
-
-        if not person.isVictim:
-            person.isVictim = True
+        if self.pos != None:
+            this_cell = self.model.grid.get_cell_list_contents([self.pos])
+            if(any(isinstance(obj, Person) for obj in this_cell)):
+                person = [obj for obj in this_cell if isinstance(obj, Person)][0]
+                if not person.isVictim:
+                    person.isVictim = True
 
 
 class Police(Entity):
@@ -26,7 +26,7 @@ class Police(Entity):
         this_cell = self.model.grid.get_cell_list_contents([self.pos])
         thief = [obj for obj in this_cell if isinstance(obj, Thief)]
         if len(thief) > 0:
-            arrest_thief = self.model.choice(thief)
+            arrest_thief = self.random.choice(thief)
             self.model.grid.remove_agent(arrest_thief)
 
 
@@ -34,6 +34,7 @@ class Person(Agent):
     def __init__(self, unique_id, pos, model):
         super().__init__(unique_id, model)
         self.isVictim = False
+        self.denounced = 0
 
     def step(self):
-        self.random_move()
+        self.denounced+=1
