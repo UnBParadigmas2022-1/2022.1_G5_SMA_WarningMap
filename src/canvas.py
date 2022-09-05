067bddee41f9a84f2132c0758f1dda9bcb087028
Thiago <thiago099carvalho@gmail.com>
2022-09-04 22:44:50 -0300

    chore: add PoliceThief model and inittial model params to server
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>
    Co-authored-by: Matheus Afonso <matheusafonsouza@gmail.com>
    Co-authored-by: Jonathan Oliveira <jonathan.jb.oliveira@gmail.com>

--- src/canvas.py
+++ src/canvas.py
@@ -3,15 +3,19 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.UserParam import UserSettableParameter
 
 from agents import Police, Thief, Person
+from models import PoliceThief
 
 def agentPortrayal(agent):
+    if agent is None:
+        return
+  
     portrayal = {
         "Filled": "true", 
         "Layer": 0, 
-        "w": 0.75, 
-        "h": 0.75
+        "w": 1, 
+        "h": 1
     }
-
+    
     if type(agent) is Person:
         if agent.isVictim:
             portrayal["Shape"] = "assets/victim.png"
@@ -21,10 +25,23 @@ def agentPortrayal(agent):
         portrayal["Shape"] = "assets/thief.jpg"
     elif type(agent) is  Police:
         portrayal["Shape"] = "assets/policeman.png"
+    
+    return portrayal
 
 canvas_element = CanvasGrid(agentPortrayal, 20, 20, 500, 500)
 
-#TO-DO: Criar model params e atualizar no server 
-server = ModularServer(, [canvas_element], "Warning Map", )
+model_params = {
+    "initial_thieves": UserSettableParameter(
+        "slider", "Quantidade inicial de ladrões", 100, 10, 300
+    ),
+    "initial_polices": UserSettableParameter(
+        "slider", "Quantidade de policiais", 50, 10, 300
+    ),
+    "initial_people": UserSettableParameter(
+        "slider", "Quantidade de civis", 50, 10, 300
+    ),
+}
+
+server = ModularServer(PoliceThief, [canvas_element], "Warning Map", model_params)
 
 server.port = 8000
